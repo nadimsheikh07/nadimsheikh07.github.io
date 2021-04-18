b2117692727991b34a67f6dce9b6abb1f0b1fe5a
nadimsheikh07 <nadim.sheikh.07@gmail.com>
2021-04-18 15:00:42 +0530

    update theme

--- component/layouts/main/index.jsx
+++ component/layouts/main/index.jsx
@@ -1,12 +1,24 @@
-import { Container } from "react-bootstrap";
+import React from "react";
+import { Container, Nav, Navbar, Button } from "react-bootstrap";
+import { MenuOutline } from "react-ionicons";
 
 const Index = ({ children }) => {
     return (
-        <div>            
+        <React.Fragment>
+            <Navbar expand={true}>                
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ml-auto">
+                    <Button variant="light">
+                    <MenuOutline/>
+                    </Button>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+
             <Container>
                 {children}
             </Container>
-        </div>
+        </React.Fragment>
     )
 }
 
--- component/resume/whyme.jsx
+++ component/resume/whyme.jsx
@@ -6,29 +6,28 @@ export const WhyMe = () => {
             <div className="heading-container">
                 <h2>Why Me</h2>
             </div>
-
-            <Container>
+        
                 <Row>
-                    <Col>
+                    <Col xs="12" sm="12" md="4" lg="4" xl="4">
                     <div className="icon">
                     <TimeOutline height="50px" width="50px"/>
                     </div>
                     <p>Operating an environment-friendly 5 Watt stakepool 24/7 with 99.9% uptime</p>
                     </Col>
-                    <Col>
+                    <Col xs="12" sm="12" md="4" lg="4" xl="4">
                     <div className="icon">
                     <WifiOutline height="50px" width="50px"/>
                     </div>
                     <p>Highly reliable because of very good Internet connection in order to get all my slots</p>
                     </Col>
-                    <Col>
+                    <Col xs="12" sm="12" md="4" lg="4" xl="4">
                     <div className="icon">
                     <ConstructOutline height="50px" width="50px"/>
                     </div>
                     <p>Running my stakepool from home, helping to truly decentralize the Cardano ecosystem</p>
                     </Col>
                 </Row>
-            </Container>
+            
         </section>
     )
 }
