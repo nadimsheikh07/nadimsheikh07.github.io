c9f83244b4f7fcd367fed639d8d3f12498a4e1d8
nadimsheikh07 <nadim.sheikh.07@gmail.com>
2023-07-24 23:21:19 +0530

    Highlights

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-import { Container, Grid, ImageList, ImageListItem, Typography } from "@mui/material";
+import { Container, Grid, ImageList, ImageListItem, List, ListItem, ListItemText, Typography } from "@mui/material";
 
 const title = "Nadim Sheikh";
 const image = "https://nadimsheikh07.github.io/image/myimage.jpg";
@@ -23,18 +23,46 @@ export default function Home() {
         <Grid item xs={4}>
           <Typography variant="h3" gutterBottom>{title}</Typography>
           <Typography variant="h5" gutterBottom>{heading}</Typography>
+          <Typography variant="h6" gutterBottom>Nadim.Sheikh.07@gmail.com</Typography>
+          <Typography variant="h6" gutterBottom>(+91)7737033665</Typography>
         </Grid>
       </Grid>
 
       <Grid container spacing={2} mt={2}>
         <Grid item xs={12}>
-          <Typography variant="h3" gutterBottom>About Me</Typography>
+          <Typography variant="h3" gutterBottom>Summary</Typography>
           <Typography variant="p" gutterBottom>Experienced full-stack engineer looking for a challenging and rewarding opportunity while using open-sourced materials to design Web-based solutions.</Typography>
           <Typography variant="p" gutterBottom>Comfortable collaborating with Front End developers project manager’s designers and database administrators in the administration of software architecture. I want to work with a well-reputed firm at a good designation.</Typography>
           <Typography variant="p" gutterBottom>Building a long-term relationship with an organization that offers me learning and growth and at the same time utilizes my potential and skills in the field I am working in.</Typography>
         </Grid>
       </Grid>
-      
+
+      <Grid container spacing={2} mt={2}>
+        <Grid item xs={12}>
+          <Typography variant="h3" gutterBottom>Highlights</Typography>
+          <List>
+            <ListItem>
+              <ListItemText primary="9+ years of accumulated talent and professional experience in web programming." />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="5+ year experience in Laravel (PHP MVC framework)." />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="3+ year experience in AdonisJs, ExpressJs (NodeJs MVC framework.)." />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="9+ year experience in MySql, MsSql, PostgreSql." />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="5+ year proficient experience in ReactJs, NextJs and Redux  (JavaScript Frameworks)." />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="3+ years experience on heroku, vercel, aws, cpanel." />
+            </ListItem>
+          </List>
+        </Grid>
+      </Grid>
+
     </Container>
   )
 }
